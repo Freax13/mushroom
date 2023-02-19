e5e8fb1af977d1c82d49de29812d5f052404541e
Tom Dohrmann <erbse.13@gmx.de>
2023-02-19 17:38:08 +0100

    implement read for `ReadonlyFile`

--- tee/kernel/src/user/process/fd/file.rs
+++ tee/kernel/src/user/process/fd/file.rs
@@ -1,19 +1,40 @@
-use crate::fs::node::FileSnapshot;
+use core::{
+    cmp,
+    sync::atomic::{AtomicUsize, Ordering},
+};
+
+use crate::{error::Result, fs::node::FileSnapshot};
 
 use super::FileDescriptor;
 
 pub struct ReadonlyFile {
     snapshot: FileSnapshot,
-    cursor_idx: u64,
+    cursor_idx: AtomicUsize,
 }
 
 impl ReadonlyFile {
     pub fn new(snapshot: FileSnapshot) -> Self {
         Self {
             snapshot,
-            cursor_idx: 0,
+            cursor_idx: AtomicUsize::new(0),
         }
     }
 }
 
-impl FileDescriptor for ReadonlyFile {}
+impl FileDescriptor for ReadonlyFile {
+    fn read(&self, buf: &mut [u8]) -> Result<usize> {
+        let idx = self
+            .cursor_idx
+            .fetch_update(Ordering::SeqCst, Ordering::SeqCst, |idx| {
+                let bytes = &self.snapshot[idx..];
+                let len = cmp::min(buf.len(), bytes.len());
+                Some(idx + len)
+            })
+            .unwrap();
+
+        let bytes = &self.snapshot[idx..];
+        let len = cmp::min(buf.len(), bytes.len());
+        buf[..len].copy_from_slice(&bytes[..len]);
+        Ok(len)
+    }
+}
