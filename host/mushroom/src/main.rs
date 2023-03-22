2544935a862a24d27b035d6839ac2ce2d4b9d3fe
Tom Dohrmann <erbse.13@gmx.de>
2023-03-22 11:13:09 +0100

    use `clap` for binary

--- host/mushroom/src/main.rs
+++ host/mushroom/src/main.rs
@@ -1,21 +1,54 @@
+use std::path::PathBuf;
+
 use anyhow::{Context, Result};
+use clap::{Args, Parser, Subcommand};
 
 fn main() -> Result<()> {
     tracing_subscriber::fmt::init();
 
-    let mut args = std::env::args().skip(1);
-    let init = args.next().context("missing init")?;
-    let input = args.next().context("missing input")?;
-    let output = args.next().context("missing output")?;
-    let attestation_report = args.next().context("missing attestation report")?;
+    let mushroom = Mushroom::parse();
+    match mushroom.subcommand {
+        MushroomSubcommand::Run(args) => run(args),
+    }
+}
 
-    let init = std::fs::read(init).context("failed to read init file")?;
-    let input = std::fs::read(input).context("failed to read input file")?;
+#[derive(Parser)]
+#[command(version)]
+struct Mushroom {
+    #[command(subcommand)]
+    subcommand: MushroomSubcommand,
+}
+
+#[derive(Subcommand)]
+enum MushroomSubcommand {
+    /// Run some code.
+    Run(RunCommand),
+}
+
+#[derive(Args)]
+struct RunCommand {
+    /// Path to the binary to run.
+    #[arg(long, value_name = "PATH")]
+    init: PathBuf,
+    /// Path to the input to process.
+    #[arg(long, value_name = "PATH")]
+    input: PathBuf,
+    /// Path to store the output.
+    #[arg(long, value_name = "PATH")]
+    output: PathBuf,
+    /// Path to store the attestation report.
+    #[arg(long, value_name = "PATH")]
+    attestation_report: PathBuf,
+}
+
+fn run(run: RunCommand) -> Result<()> {
+    let init = std::fs::read(run.init).context("failed to read init file")?;
+    let input = std::fs::read(run.input).context("failed to read init file")?;
 
     let result = mushroom::main(&init, &input)?;
 
-    std::fs::write(output, result.output).context("failed to write output")?;
-    std::fs::write(attestation_report, result.attestation_report)
+    std::fs::write(run.output, result.output).context("failed to write output")?;
+    std::fs::write(run.attestation_report, result.attestation_report)
         .context("failed to write attestation report")?;
 
     Ok(())
