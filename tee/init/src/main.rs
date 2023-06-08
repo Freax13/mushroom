16f95f2c9e15dbe9ced96102278efa3ac612a970
Tom Dohrmann <erbse.13@gmx.de>
2023-06-08 13:36:44 +0200

    unpack tar & run busybox in init

--- tee/init/src/main.rs
+++ tee/init/src/main.rs
@@ -1,26 +1,21 @@
-use std::{fs::OpenOptions, os::unix::prelude::OpenOptionsExt, process::Command};
+use std::{fs::File, process::Command};
 
-use anyhow::{Context, Result};
+use anyhow::Result;
+use tar::Archive;
 
 fn main() -> Result<()> {
-    let this = std::env::args().next().unwrap();
-    if this == "/bin/init" {
-        let mut input = std::fs::File::open("/bin/init").context("failed to open input file")?;
-        let mut output = OpenOptions::new()
-            .write(true)
-            .create(true)
-            .mode(0o755)
-            .open("/bin/proc")
-            .context("failed to create output file")?;
-        std::io::copy(&mut input, &mut output).context("failed to copy")?;
+    let root = "/";
 
-        let status = Command::new("/bin/proc").status().unwrap();
-        assert!(status.success());
-    } else {
-        let mut input = std::fs::File::open("/dev/input").context("failed to open input file")?;
-        let mut output =
-            std::fs::File::create("/dev/output").context("failed to create output file")?;
-        std::io::copy(&mut input, &mut output).context("failed to copy")?;
-    }
+    // Unpack tar archive.
+    let file = File::open("/dev/input")?;
+    let mut archive = Archive::new(file);
+    archive.unpack(root)?;
+
+    // Execute busybox.
+    let status = Command::new("/bin/busybox")
+        .stdout(File::create("/dev/output")?)
+        .status()?;
+    assert!(status.success());
+
     Ok(())
 }
