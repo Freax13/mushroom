d9d01982ee59d7043016acd96aacf75375f84491
Tom Dohrmann <erbse.13@gmx.de>
2023-06-11 11:42:21 +0200

    change init to run gcc

--- tee/init/src/main.rs
+++ tee/init/src/main.rs
@@ -6,16 +6,27 @@ use tar::Archive;
 fn main() -> Result<()> {
     let root = "/";
 
+    std::fs::remove_dir_all("/bin")?;
+
     // Unpack tar archive.
     let file = File::open("/dev/input")?;
     let mut archive = Archive::new(file);
     archive.unpack(root)?;
 
+    std::fs::write("/test.c", "int main() { return 1; }")?;
+
     // Execute busybox.
-    let status = Command::new("/bin/busybox")
-        .stdout(File::create("/dev/output")?)
+    let status = Command::new("/usr/bin/gcc")
+        .arg("/test.c")
+        .arg("-o")
+        .arg("test")
+        .arg("-v")
         .status()?;
     assert!(status.success());
+
+    let mut output = File::create("/dev/output")?;
+    let mut res = File::open("test")?;
+    std::io::copy(&mut res, &mut output)?;
 
     Ok(())
 }
