b0a2c7e0e042dabfb65bb31b17fc7f64794da61e
Tom Dohrmann <erbse.13@gmx.de>
2023-06-25 21:15:01 +0200

    print stack frames when panicking

--- tee/kernel/src/panic.rs
+++ tee/kernel/src/panic.rs
@@ -1,17 +1,20 @@
 use core::{
+    arch::asm,
     panic::PanicInfo,
     sync::atomic::{AtomicBool, Ordering},
 };
 
-use log::error;
 use x86_64::structures::idt::InterruptDescriptorTable;
 
 use crate::host::exit;
 
 #[panic_handler]
 fn panic_handler(info: &PanicInfo) -> ! {
-    #[cfg(debug_assertions)]
+    let _ = info;
+
+    #[cfg(not(feature = "harden"))]
     {
+        use log::error;
         static IS_PANICKING: AtomicBool = AtomicBool::new(false);
         let is_already_panicking = IS_PANICKING.swap(true, Ordering::SeqCst);
 
@@ -21,6 +24,8 @@ fn panic_handler(info: &PanicInfo) -> ! {
         }
 
         error!("{info}");
+
+        print_backtrace();
     }
 
     exit(false);
@@ -39,5 +44,39 @@ fn triple_fault() -> ! {
         unsafe {
             core::arch::asm!("int3");
         }
+    }
+}
+
+#[cfg(not(feature = "harden"))]
+#[inline(always)]
+fn walk_frames() -> impl Iterator<Item = u64> {
+    use core::iter::from_fn;
+
+    let rbp: u64;
+    unsafe {
+        asm!("mov {}, rbp", out(reg) rbp);
+    }
+
+    let mut frame_pointer = rbp;
+    from_fn(move || {
+        if frame_pointer == 0 {
+            return None;
+        }
+
+        let copy = frame_pointer;
+        frame_pointer = unsafe { (frame_pointer as *const u64).read_volatile() };
+        Some(copy)
+    })
+}
+
+#[cfg(not(feature = "harden"))]
+#[inline(always)]
+fn print_backtrace() {
+    use log::debug;
+
+    debug!("Stack frames:");
+    for frame in walk_frames() {
+        let rip = unsafe { (frame as *const u64).add(1).read_volatile() };
+        debug!("{rip:016x}");
     }
 }
