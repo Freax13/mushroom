ba174e487a61fc72beeda28126002262c9718e4a
Tom Dohrmann <erbse.13@gmx.de>
2024-03-12 09:16:25 +0000

    add test for signal handling

--- tee/tests/src/lib.rs
+++ tee/tests/src/lib.rs
@@ -1,9 +1,21 @@
 #![cfg(test)]
 #![feature(asm_const)]
 
-use std::arch::asm;
+use std::{
+    arch::asm,
+    ffi::c_void,
+    mem::size_of,
+    ptr::NonNull,
+    sync::atomic::{AtomicBool, AtomicU8, Ordering},
+};
 
-use libc::{SYS_exit, SYS_vfork};
+use nix::{
+    libc::{siginfo_t, SYS_exit, SYS_vfork},
+    sys::{
+        mman::{mprotect, ProtFlags},
+        signal::{sigaction, SaFlags, SigAction, SigSet},
+    },
+};
 
 #[test]
 fn it_works() {
@@ -44,4 +56,63 @@ fn vfork_exit() {
             exit = const SYS_exit,
         );
     }
+}
+
+#[test]
+fn signal_handling() {
+    // Some memory for us to mess with.
+    #[repr(align(4096))]
+    struct Memory {
+        value: AtomicU8,
+    }
+    static SOME_MEMORY: Memory = Memory {
+        value: AtomicU8::new(0),
+    };
+
+    /// Whether we handled a signal.
+    static HANDLED: AtomicBool = AtomicBool::new(false);
+
+    // Take away the write permisssions for SOME_MEMORY.
+    unsafe {
+        mprotect(
+            NonNull::from(&SOME_MEMORY).cast(),
+            size_of::<Memory>(),
+            ProtFlags::PROT_READ,
+        )
+        .unwrap();
+    }
+
+    extern "C" fn handler(_: i32, _: *mut siginfo_t, _: *mut c_void) {
+        // Restore the write permissions.
+        unsafe {
+            mprotect(
+                NonNull::from(&SOME_MEMORY).cast(),
+                size_of::<Memory>(),
+                ProtFlags::PROT_READ | ProtFlags::PROT_WRITE,
+            )
+            .unwrap();
+        }
+
+        // Store that we handled a signal.
+        HANDLED.store(true, Ordering::SeqCst);
+    }
+    // Register the signal handler.
+    unsafe {
+        sigaction(
+            nix::sys::signal::Signal::SIGSEGV,
+            &SigAction::new(
+                nix::sys::signal::SigHandler::SigAction(handler),
+                SaFlags::empty(),
+                SigSet::empty(),
+            ),
+        )
+        .unwrap();
+    }
+
+    assert!(!HANDLED.load(Ordering::SeqCst));
+
+    // Write to SOME_MEMORY to trigger a segfault.
+    SOME_MEMORY.value.fetch_add(1, Ordering::SeqCst);
+
+    assert!(HANDLED.load(Ordering::SeqCst));
 }
