77da21804ddf171b5e109b813c21d17a6b1b53e4
Tom Dohrmann <erbse.13@gmx.de>
2024-03-14 11:34:16 +0000

    fix rename

--- tee/kernel/src/fs/node.rs
+++ tee/kernel/src/fs/node.rs
@@ -412,7 +412,7 @@ pub fn rename(
         PathSegment::FileName(filename) => filename,
     };
 
-    if !Arc::ptr_eq(&old_parent, &new_parent) && newname == oldname {
+    if !Arc::ptr_eq(&old_parent, &new_parent) || newname != oldname {
         let node = old_parent.get_node(&oldname, ctx)?;
         new_parent.mount(newname.into_owned(), node)?;
         old_parent.delete_non_dir(oldname.into_owned())?;
