20cba6bc043d6b7683dc6027c0d22a898c6e98b6
Tom Dohrmann <erbse.13@gmx.de>
2025-06-01 15:32:38 +0000

    increase max string size for debug printing

--- tee/kernel/src/user/process/syscall/args/pointee.rs
+++ tee/kernel/src/user/process/syscall/args/pointee.rs
@@ -305,7 +305,7 @@ impl Pointee for CStr {
     fn display(f: &mut dyn fmt::Write, addr: VirtAddr, thread: &ThreadGuard) -> fmt::Result {
         let res = thread
             .virtual_memory()
-            .read_cstring(Pointer::from(addr), 128);
+            .read_cstring(Pointer::from(addr), 1024);
         match res {
             Ok(value) => write!(f, "{value:?}"),
             Err(_) => write!(f, "{:#x} (invalid ptr)", addr.as_u64()),
