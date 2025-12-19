36006a702564b251998576e8df2cf763f56747a3
Tom Dohrmann <erbse.13@gmx.de>
2025-12-19 09:24:38 +0100

    add unit tests for pty exlusive mode

--- tee/tests/src/pty.rs
+++ tee/tests/src/pty.rs
@@ -4,7 +4,10 @@ use nix::{
     Result,
     errno::Errno,
     fcntl::{OFlag, open},
-    libc::{Ioctl, TCFLSH, TCIFLUSH, TCIOFLUSH, TCOFLUSH, TIOCGPTN, TIOCSPTLCK, c_int, ioctl},
+    libc::{
+        Ioctl, TCFLSH, TCIFLUSH, TCIOFLUSH, TCOFLUSH, TIOCEXCL, TIOCGEXCL, TIOCGPTN, TIOCNXCL,
+        TIOCSPTLCK, c_int, ioctl,
+    },
     sys::stat::Mode,
     unistd::{read, write},
 };
@@ -162,4 +165,90 @@ fn flush_input_output_slave_output() {
     let mut buf = [0; 16];
     assert_eq!(read(&master, &mut buf), Ok(5));
     assert_eq!(buf[..5], *b"DEF\r\n");
+}
+
+/// Enable exclusive mode.
+fn tiocexcl(fd: &OwnedFd) -> Result<()> {
+    let res = unsafe { ioctl(fd.as_raw_fd(), TIOCEXCL as Ioctl) };
+    Errno::result(res)?;
+    Ok(())
+}
+
+/// Query whether exclusive mode is enabled.
+fn tiocgexcl(fd: &OwnedFd) -> Result<bool> {
+    let mut enabled = 0i32;
+    let res = unsafe { ioctl(fd.as_raw_fd(), TIOCGEXCL as Ioctl, &mut enabled) };
+    Errno::result(res)?;
+    Ok(enabled != 0)
+}
+
+/// Disable exclusive mode.
+fn tiocnxcl(fd: &OwnedFd) -> Result<()> {
+    let res = unsafe { ioctl(fd.as_raw_fd(), TIOCNXCL as Ioctl) };
+    Errno::result(res)?;
+    Ok(())
+}
+
+#[test]
+fn enable_exclusive_mode_master() {
+    let (master, slave) = make_pty_pair();
+
+    // Both sides start out as unlocked.
+    assert_eq!(tiocgexcl(&master), Ok(false));
+    assert_eq!(tiocgexcl(&slave), Ok(false));
+
+    // Enable exclusive mode for the master.
+    assert_eq!(tiocexcl(&master), Ok(()));
+
+    // The master is locked.
+    assert_eq!(tiocgexcl(&master), Ok(true));
+    // But this doesn't do much because the master can't be opened again
+    // anyway.
+
+    // The slave is not locked.
+    assert_eq!(tiocgexcl(&slave), Ok(false));
+    // So it can be opened.
+    let idx = tiocgptn(&master);
+    assert!(open(&*format!("/dev/pts/{idx}"), OFlag::O_RDWR, Mode::empty()).is_ok());
+
+    // Disable exclusive mode for the master.
+    assert_eq!(tiocnxcl(&master), Ok(()));
+
+    // Now, both sides are unlocked again.
+    assert_eq!(tiocgexcl(&master), Ok(false));
+    assert_eq!(tiocgexcl(&slave), Ok(false));
+}
+
+#[test]
+fn enable_exclusive_mode_slave() {
+    let (master, slave) = make_pty_pair();
+
+    // Both sides start out as unlocked.
+    assert_eq!(tiocgexcl(&master), Ok(false));
+    assert_eq!(tiocgexcl(&slave), Ok(false));
+
+    // Enable exlusive mode for the slave.
+    assert_eq!(tiocexcl(&slave), Ok(()));
+
+    // The master is not locked.
+    assert_eq!(tiocgexcl(&master), Ok(false));
+
+    // The slave is locked.
+    assert_eq!(tiocgexcl(&slave), Ok(true));
+    // So it can't be opened.
+    let idx = tiocgptn(&master);
+    assert_eq!(
+        open(&*format!("/dev/pts/{idx}"), OFlag::O_RDWR, Mode::empty()).unwrap_err(),
+        Errno::EBUSY
+    );
+
+    // Disable exclusive mode for the slave.
+    assert_eq!(tiocnxcl(&slave), Ok(()));
+
+    // Now, both sides are unlocked again.
+    assert_eq!(tiocgexcl(&master), Ok(false));
+    assert_eq!(tiocgexcl(&slave), Ok(false));
+
+    // And now the slave can be opened again.
+    assert!(open(&*format!("/dev/pts/{idx}"), OFlag::O_RDWR, Mode::empty()).is_ok());
 }
